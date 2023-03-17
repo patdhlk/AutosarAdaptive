87d9ca4290302d8a2ded84303d6c68dfe39e7d9b
Patrick D <patrick.dahlke@pichu.io>
2023-03-17 16:05:35 +0100

    Fix plantuml build

--- conf.py
+++ conf.py
@@ -35,12 +35,12 @@ html_css_files = [
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
-if on_rtd:
-    plantuml = 'java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
-else:
-    plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar")
+#if on_rtd:
+    #plantuml = 'java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
+#else:
+plantuml = 'java -jar %s' % os.path.join(os.path.dirname(__file__), "utils", "plantuml.jar")
 
-    plantuml_output_format = 'svg'
+plantuml_output_format = 'svg'
 
 
 templates_path = ['_templates']
